09f35b5698c70c20ad71dc958154e3312064bcf0
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2022-05-22 21:26:21 +0800

    Add comments

--- helper/ps1/InitProject.ps1
+++ helper/ps1/InitProject.ps1
@@ -1,3 +1,6 @@
+<#
+    Variables
+#>
 $Readme = @(
     "# Project`n"
     "## Setup"
@@ -5,13 +8,18 @@ $Readme = @(
 ) -join "`n"
 $ProjectFolder = Read-Host "Input project folder"
 
+<#
+    Main
+#>
 if (Test-Path -Path $ProjectFolder) {
+    # Exit if project folder exist & not empty
     if ((Get-ChildItem $ProjectFolder | Measure-Object).Count -ne 0) {
         Write-Host "Folder not empty"
         exit 1
     }
 }
 else {
+    # Create project folder if not exist
     New-Item -Path $ProjectFolder -ItemType "directory"
 }
 
