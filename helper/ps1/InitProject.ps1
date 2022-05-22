c0f6459e2f32d3d67838c7eea99a2148b5784384
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2022-05-22 20:13:13 +0800

    Exit if project folder not empty

--- helper/ps1/InitProject.ps1
+++ helper/ps1/InitProject.ps1
@@ -1,7 +1,10 @@
 $ProjectFolder = Read-Host "Input project folder"
 
 if (Test-Path -Path $ProjectFolder) {
-
+    if ((Get-ChildItem $ProjectFolder | Measure-Object).Count -ne 0) {
+        Write-Host "Folder not empty"
+        exit 1
+    }
 }
 else {
     New-Item -Name $ProjectFolder -ItemType "directory"
