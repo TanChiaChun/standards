5332d052c2f1c8f579da4afa44a7bc5d5170cf53
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2022-05-22 19:55:05 +0800

    Create project folder if not exist

--- helper/ps1/InitProject.ps1
+++ helper/ps1/InitProject.ps1
@@ -1,3 +1,10 @@
 $ProjectFolder = Read-Host "Input project folder"
 
+if (Test-Path -Path $ProjectFolder) {
+
+}
+else {
+    New-Item -Name $ProjectFolder -ItemType "directory"
+}
+
 New-Item -Path $ProjectFolder -Name ".gitignore" -ItemType "file" -Value "`n"
